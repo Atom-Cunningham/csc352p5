7b05e66c9cf118db143c6ff6e0db7fabd3aeeb92
honestus-codice <laser@email.arizona.edu>
2019-12-09 22:50:57 -0700

    Update yahtzee.c

--- yahtzee.c
+++ yahtzee.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define LOWER = 1;
-#define UPPER = 6;
+#include <ctype.h>
 
 
 /**
@@ -33,13 +31,16 @@ int get_random_die(){
 /**
  * read roll
  */
-int read_roll_nodrvr(int dice[]){
+
+/**populates an int array of size
+ * for playing outsized the kernel only
+ */
+int read_roll_nodrvr(int size, int dice[]){
     int i;
     int rand;
-    for(i = 0; i < 6; i++){
+    for(i = 0; i < size; i++){
         rand = get_random_die();
         dice[i] = rand;
-        printf("%s %d = %d\n", "random", i, rand);
     }
     return 0;
 }
@@ -52,11 +53,18 @@ int read_roll_nodrvr(int dice[]){
  * takes the size of the array, and an int array
  * returns 0
  */
-void print_int_array(int max, int dice[]){
-    printf("Your roll:\n\n");
+void print_int_array(int size, int dice[], int attempt){
+    char * count = "";
+    if(attempt == 2){
+        count = "second ";
+    }
+    if (attempt == 3){
+        count = "third ";
+    }
+    printf("Your %s roll:\n\n", count);
 
     int i;
-    for(i = 0; i < max-1; i++){
+    for(i = 0; i < size-2; i++){
         printf("%d ", dice[i]);
     }
     //print last die with newline
@@ -68,11 +76,34 @@ void print_int_array(int max, int dice[]){
  * user input
  */
 
+/**takes user input, the index+1 of dice to reroll
+ */
+int reroll(int * dice){
+
+    int idx;
+    int i = 0;
+    scanf("die to reroll %d", idx);
+    for(i = 0; idx !=0 && i < 5; i++){
+        printf("die roll idx %d\ndie = %d", idx, dice[idx]);
+        if(idx == 0){
+            break;
+        }
+        dice[idx] = get_random_die();
+        scanf("die to reroll %d", idx);
+    }
+    return 0;
+}
+
+/**returns true if the integer is between 0 and 6 inclusive
+ */
+int is_valid_die_idx(int die){
+    return die <= 0 && die <7;
+}
 
 int main(int argc, char ** argv){
     int dice[5];
 
-    read_roll_nodrvr(dice);
+    read_roll_nodrvr(6, dice);
     print_int_array(6, dice);
     return 0;
 }
