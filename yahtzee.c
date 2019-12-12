cc419c165c8bdf2ffa08f24861c951c180edc75a
honestus-codice <laser@email.arizona.edu>
2019-12-11 18:07:30 -0700

    Update yahtzee.c

--- yahtzee.c
+++ yahtzee.c
@@ -349,7 +349,7 @@ int lower_entry(int * dice,int * section){
     //get selection
     int selection = get_section(7) - '0';
     int idx = selection-1;
-    while(section[idx] < 0){//ask until blank space is found
+    while(section[idx] >= 0){//ask until blank space is found
         printf("That selection has already been made.\n");
         selection = get_section(7);
         idx = selection-1;
